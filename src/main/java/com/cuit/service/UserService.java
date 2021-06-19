d850546f585deeda168477112744e4bbf9b93f98
wr1sw <840265545@qq.com>
2021-06-20 01:19:56 +0800

    wr1sw:增添了用户service

--- src/main/java/com/cuit/service/UserService.java
+++ src/main/java/com/cuit/service/UserService.java
@@ -1,23 +1,7 @@
 package com.cuit.service;
 
+import com.cuit.base.BaseService;
 import com.cuit.pojo.User;
 
-import java.util.List;
-
-public interface UserService {
-
-    int deleteByPrimaryKey(Integer userId);
-
-    int insert(User record);
-
-    int insertSelective(User record);
-
-    User selectByPrimaryKey(Integer userId);
-
-    List<User> selectAllUsers();
-
-    int updateByPrimaryKeySelective(User record);
-
-    int updateByPrimaryKey(User record);
-
+public interface UserService extends BaseService<User> {
 }
