ddfee095be995085678f5c7f20b3c1a43ec7d1f7
wr1sw <840265545@qq.com>
2021-06-22 22:06:06 +0800

    wr1sw:增添了常量表和用户名和id

--- src/main/java/com/cuit/utils/Consts.java
+++ src/main/java/com/cuit/utils/Consts.java
@@ -1,7 +1,39 @@
 package com.cuit.utils;
 
-public class Consts {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Consts {
+    /**
+     * 二级分类常量表
+     */
+    public static final Map<Integer, String> CATEGORY= new HashMap<Integer, String>(){
+        {
+            put(1,"手机壳");
+            put(9,"手机");
+            put(16,"电脑");
+            put(27,"鼠标");
+            put(36,"键盘");
+            put(45,"耳机");
+            put(55,"平板");
+        }
+    };
     /**用户实体的名称*/
     public static final String MANAGE = "manage";
+
+    /**
+     * 前端用户登录user
+     */
+    public static final String USERNAME = "userName";
+
+    /**
+     * 前端用户登录id
+     */
+
+    public static final String USERID = "userId";
+
+    /**
+     * 错误码
+     */
+    public static final String RES = "res";
 }
