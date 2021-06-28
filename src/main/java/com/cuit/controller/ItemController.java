5c7538d9fd92fac870da3b47ce0168172d900cc7
wr1sw <840265545@qq.com>
2021-06-28 18:41:54 +0800

    wr1sw:修改细节bug

--- src/main/java/com/cuit/controller/ItemController.java
+++ src/main/java/com/cuit/controller/ItemController.java
@@ -2,16 +2,24 @@ package com.cuit.controller;
 
 import com.cuit.base.BaseController;
 import com.cuit.pojo.Item;
+import com.cuit.pojo.ItemCategory;
+import com.cuit.service.ItemCategoryService;
 import com.cuit.service.ItemService;
 import com.cuit.utils.Pager;
 import com.cuit.utils.StringUtils;
+import com.cuit.utils.SystemContext;
+import com.cuit.utils.UUIDUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.commons.CommonsMultipartFile;
 
+import javax.servlet.http.HttpServletRequest;
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 @Controller
@@ -20,6 +28,9 @@ public class ItemController extends BaseController {
 
     @Autowired
     private ItemService itemService;
+
+    @Autowired
+    private ItemCategoryService itemCategoryService;
     @Autowired
     private StringUtils stringUtils;
 
@@ -38,6 +49,99 @@ public class ItemController extends BaseController {
         model.addAttribute("pagers",pagers);
         model.addAttribute("obj",item);
         return "item/item";
+    }
+
+    /**
+     * 添加商品入口
+     * by wr1sw
+     */
+    @RequestMapping("/add")
+    public String add(Model model){
+        String sql = "select * from item_category where isDelete = 0 and pid is not null order by id";
+        List<ItemCategory> listBySqlReturnEntity = itemCategoryService.listBySqlReturnEntity(sql);
+        model.addAttribute("types",listBySqlReturnEntity);
+        return "item/add";
+    }
+
+    /**
+     * 执行添加商品
+     * by wr1sw
+     */
+    @RequestMapping("/exAdd")
+    public String exAdd(Item item, @RequestParam("file") CommonsMultipartFile[] files, HttpServletRequest request) throws IOException {
+        itemCommon(item, files, request);
+        item.setGmNum(0);
+        item.setIsDelete(0);
+        item.setScNum(0);
+        itemService.insert(item);
+        return "redirect:/Item/findBySql";
+    }
+
+    /**
+     * 修改商品入口
+     */
+    @RequestMapping("/update")
+    public String update(Integer id,Model model){
+        Item obj = itemService.load(id);
+        String sql = "select * from item_category where isDelete = 0 and pid is not null order by id";
+        List<ItemCategory> listBySqlReturnEntity = itemCategoryService.listBySqlReturnEntity(sql);
+        model.addAttribute("types",listBySqlReturnEntity);
+        model.addAttribute("obj",obj);
+        return "item/update";
+    }
+
+    /**
+     * 执行修改商品
+     */
+    @RequestMapping("/exUpdate")
+    public String exUpdate(Item item, @RequestParam("file")CommonsMultipartFile[] files, HttpServletRequest request) throws IOException {
+        itemCommon(item, files, request);
+        itemService.updateById(item);
+        return "redirect:/Item/findBySql";
+    }
+
+    /**
+     * 新增和更新的公共方法
+     */
+    private void itemCommon(Item item, @RequestParam("file") CommonsMultipartFile[] files, HttpServletRequest request) throws IOException {
+        if(files.length>0) {
+            for (int s = 0; s < files.length; s++) {
+                String n = UUIDUtils.create();
+                String path = SystemContext.getRealPath() + "\\static\\images\\GoodsPicture\\" + n + files[s].getOriginalFilename();
+                File newFile = new File(path);
+                //通过CommonsMultipartFile的方法直接写文件
+                files[s].transferTo(newFile);
+                if (s == 0) {
+                    item.setUrl1(request.getContextPath()+"\\static\\images\\GoodsPicture\\" + n + files[s].getOriginalFilename());
+                }
+                if (s == 1) {
+                    item.setUrl2(request.getContextPath()+"\\static\\images\\GoodsPicture\\" + n + files[s].getOriginalFilename());
+                }
+                if (s == 2) {
+                    item.setUrl3(request.getContextPath()+"\\static\\images\\GoodsPicture\\" + n + files[s].getOriginalFilename());
+                }
+                if (s == 3) {
+                    item.setUrl4(request.getContextPath()+"\\static\\images\\GoodsPicture\\" + n + files[s].getOriginalFilename());
+                }
+                if (s == 4) {
+                    item.setUrl5(request.getContextPath()+"\\static\\images\\GoodsPicture\\" + n + files[s].getOriginalFilename());
+                }
+            }
+        }
+        ItemCategory byId = itemCategoryService.getById(item.getCategoryIdTwo());
+        item.setCategoryIdOne(byId.getPid());
+    }
+
+    /**
+     * 商品下架
+     * by wr1sw
+     */
+    @RequestMapping("/delete")
+    public String update(Integer id){
+        Item obj = itemService.load(id);
+        obj.setIsDelete(1);
+        itemService.updateById(obj);
+        return "redirect:/Item/findBySql";
     }
     /**
      * Create by Miracle
@@ -65,7 +169,7 @@ public class ItemController extends BaseController {
         Pager<Item> pagers = itemService.findBySqlRerturnEntity(sql);
 //        int pagersSize = pagers.getDatas().size();
         int size = 0;
-        size = (int) ((int)pagers.getDatas().size()/2.35);
+        size = (int) ((int)pagers.getDatas().size()/3);
 //        if(pagersSize>=13){
 //            size = 6;
 //        }else{
